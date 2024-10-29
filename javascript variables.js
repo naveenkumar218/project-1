ba8199d274d98f00d2c60cd194d07a3c44c90f92
naveenkumar218 <mnaveenmurali671@gmail.com>
2024-10-29 15:21:59 +0530

    feat : added variable file

---
+++ javascript variables.js
@@ -0,0 +1,40 @@
+/**WHAT IS VARIABLES ?
+variables are the containers of storing Data .**/
+
+/**Variables can be in three types : 1)var
+                                  2)let
+                                 3)connst **/
+
+
+/**1) var: .it is a function scope.
+.it is accessible throughout the function.
+.it can be both assigned and redclared.
+
+example: if (true) {
+        var x = 10;
+
+    }
+console.log(x);**/
+
+/**2)LET: are the containers of storing Data .
+. it is blocked Scope . 
+. it can be reassigned but not redeclared. 
+. it is accessible within the bracket. 
+
+example: if (true) {
+        let a = 5;
+        console.log(a);
+    }
+console.log(a);**/
+
+/**3) CONST: .it is blocked scope . 
+.it is accessible within the block. 
+.it cannot be reassign and redeclared. 
+
+example :  if (true){
+
+const c= 100;
+console.log(c);
+}
+
+console.log(c); **/
