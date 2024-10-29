2b65e35ee93769e4ca9ecc47f73d19419ab3c10d
naveenkumar218 <mnaveenmurali671@gmail.com>
2024-10-29 14:35:02 +0530

    feat : added js file

---
+++ java tutorial.js
@@ -0,0 +1,33 @@
+/**what is javascript ?
+    1 - javascript is the world most popular programming language.
+2 - javascript is the programming language of web.
+3 - javascript is easy to learn.
+example:
+    <
+    html >
+    <
+    body >
+    <
+    h2 > my irst javascript < /h2> <
+    button type = "button"
+onclick = "document.getElementById('demo').innerHTML = Dae()" > < /button> <
+    p id = "demo" > < /p> <
+    /body> <
+    /html>**/
+
+/**javascript syntax?  
+ javascript sntax is the set of rules, how javascript program are constructed.
+  
+ It defines two types of value: 1) Literals- numbers,string.
+     2) Variables         **/
+
+/**JAVASCRIPT COMMENTS?
+ javascript comments can be used to explain js code and make it easy to readeable.
+
+ Two types: 1)single line comments.
+            2)multi-line comments.**/
+
+
+/**JAVASCRIPT DATATYPES?   
+  two types- 1)primitive datatypes: are the most basic datatypes.-string, number, boolean, undefined, biginit, symbol, null.
+             non-primitive datatypes: are the more complex and can hold collection of values.- object, array.**/
